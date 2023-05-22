caede425c1309a9fcdefc9f32b253cc15c0db8a1
danieldawsonjensen <dani540c@gmail.com>
2023-05-22 19:06:13 +0200

    first commit

---
+++ AuctionServiceApi.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace AuctionServiceApi.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
