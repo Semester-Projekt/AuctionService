9e3be86aab1061f717c6578b201a4ecac44365cc
danieldawsonjensen <dani540c@gmail.com>
2023-05-29 19:41:14 +0200

    added version endpoint

--- AuctionServiceApi/Controllers/AuctionController.cs
+++ AuctionServiceApi/Controllers/AuctionController.cs
@@ -76,8 +76,38 @@ public class AuctionController : ControllerBase
     //RabbitMQ slut
 
 
+    // VERSION_ENDEPUNKT
+    [HttpGet("version")]
+    public async Task<Dictionary<string, string>> GetVersion()
+    {
+        var properties = new Dictionary<string, string>();
+        var assembly = typeof(Program).Assembly;
+
+        properties.Add("service", "Auction");
+        var ver = FileVersionInfo.GetVersionInfo(typeof(Program).Assembly.Location).ProductVersion;
+        properties.Add("version", ver!);
+
+        try
+        {
+            var hostName = System.Net.Dns.GetHostName();
+            var ips = await System.Net.Dns.GetHostAddressesAsync(hostName);
+            var ipa = ips.First().MapToIPv4().ToString();
+            properties.Add("hosted-at-address", ipa);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message);
+            properties.Add("hosted-at-address", "Could not resolve IP-address");
+        }
+
+        return properties;
+    }
 
 
+
+
+    
+    
     //GET
     [HttpGet("getallauctions")]
     public async Task<IActionResult> GetAllAuctions()
